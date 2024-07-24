51e037859b89234cb5aa1840ab2c5b8147f1f0bf
dmal0 <143022794+dmal0@users.noreply.github.com>
2024-07-23 21:06:06 -0600

    Update sa.py

--- sa.py
+++ sa.py
@@ -21,6 +21,7 @@ print("* All libraries have been successfull imported")
 data = pd.read_csv("sexual_orientation_auer_anonymized.csv")
 
 # list of independent variables 
+''' # Simplified below for basic testing purposes
 X = data[['sex (1=MtF; 2 =FtM)',
 'initial_sex_orientation (1= androphilic; 2 =gynephilic; 3 = bisexual, 4 = analloerotic)', 
 'age_of_onset',
@@ -35,6 +36,11 @@ X = data[['sex (1=MtF; 2 =FtM)',
 'type_of_surgery (1 = hysterectomy + mastectomy; 2 = +penoid, 3 = neovagina, 4 = breast augmentation)', 
 'direction_change (direction of change in sexual orientation; 1= androphilic to gynephilic;  2=androphilic to bisexual; 3=gynephilic to androphilic; 4=gynephilic to bisexual, 5 = analloerotic to gynephilic; 6 =analloerotic to androphilic; 7 = analloerotic to bisexual; 8 = gynephilic to analloerotic) ',
 'interval_horm_surg (interval from initiation of hormone therapy to sex reassignement surgery)']]
+'''
+
+X = data[['sex (1=MtF; 2 =FtM)',
+'hormontherapy (1 =yes; 2 =no)',
+'sex reassignement surgery (1= yes; 2 = no)',]]
 
 # dependent variable
 y = data['changesexorient (there has been a change in self-reported sexual orientation: 1= yes; 2 = no)']
@@ -43,7 +49,7 @@ y = data['changesexorient (there has been a change in self-reported sexual orien
 #data[''] = data[''].replace('NA', '-1')
 # See: https://stackoverflow.com/questions/38117016/update-pandas-dataframe-with-str-replace-vs-replace
 
-'''
+''' # Loe notes to self
 > ignore NAs
   age_of_onset -> can ignore NAs because they will probably throw off predictions
 
@@ -63,6 +69,7 @@ y = data['changesexorient (there has been a change in self-reported sexual orien
 print("* NA and empty values have been successfully handled")
 
 # refine variables after handling NA 
+# not sure what you mean here? -Loe
 
 # perform regression 
 # regr = linear_model.LinearRegression()
