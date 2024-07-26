e01f8810bf78557dfa6f97a338ca0e14fc786cd6
Sam Allen <164280939+valeriabounze1688@users.noreply.github.com>
2024-07-26 10:24:38 +0000

    Calculate correlations between variables and performed z-test; commented out results and interpretations.

--- sa.py
+++ sa.py
@@ -6,20 +6,24 @@
 # import libraries 
 import pandas as pd 
 import numpy as np 
-import matplotlib.pyplot as plt
-import seaborn as sns
+# import matplotlib.pyplot as plt # Optional to use - for visualizations
+# import seaborn as sns # Optional to use - for visualizations
+from statsmodels.stats.proportion import proportions_ztest # To perform z-test
 
 # from scikit-learn 
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
 from sklearn import linear_model
+from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
 
 print("* All libraries have been successfull imported")
 
 # load in data
-# LOE: In this copy of the data, I replaced/filled NA/blank with "0"
-data = pd.read_csv("data_simplified.csv")
+# LOE: data_simplified_preclean.csv is data_simplified but
+#      without all NA/empty values manually replaced to be 0.
+#      It is the same as sexual_orientation_auer_anonymized.csv,
+#      but without the rows of empty cells
+data = pd.read_csv("data_simplified_preclean.csv")
 
 # list of independent variables 
 '''
@@ -41,14 +45,6 @@ X = data[['sex (1=MtF; 2 =FtM)',
 'interval_horm_surg (interval from initiation of hormone therapy to sex reassignement surgery)']]
 '''
 
-X = data[['sex (1=MtF; 2 =FtM)',
-          'initial_sex_orientation (1= androphilic; 2 =gynephilic; 3 = bisexual, 4 = analloerotic)',
-          'hormontherapy (1 =yes; 2 =no)',
-          'sex reassignement surgery (1= yes; 2 = no)']]
-
-# dependent variable
-y = data['changesexorient (there has been a change in self-reported sexual orientation: 1= yes; 2 = no)']
-
 ''' # LOE: Notes to Self
 # > Ignore NAs
 #   age_of_onset -> can maybe ignore NAs because they will probably throw off predictions? or just 0 fill
@@ -66,29 +62,37 @@ y = data['changesexorient (there has been a change in self-reported sexual orien
 #   direction_change
 '''
 
-# replace NA values with a -1 to ensure consistency with categorical variables
+X = data[['sex (1=MtF; 2 =FtM)',
+          'initial_sex_orientation (1= androphilic; 2 =gynephilic; 3 = bisexual, 4 = analloerotic)',
+          'hormontherapy (1 =yes; 2 =no)',
+          'sex reassignement surgery (1= yes; 2 = no)']]
+
+# dependent variable
+y = data['changesexorient (there has been a change in self-reported sexual orientation: 1= yes; 2 = no)']
+
+# Clean the data
+# Fill NA/empty with 0 because the numbers in the selected
+# columns used in X are categorical; also, dropping NA values
+# vastly reduces the number of rows from 115 to 15
+#print(data) # Test print
+X = X.fillna(0)         # Get X
+y = y.fillna(0)         # Get y
+data = data.fillna(0)   # Get anything left over
+#print(data) # Test print
+
+# LOE: Ignore NAs completely via dropping
+# Also works in replacing/dropping but vastly reduces number of tuples;
+# drops from 115 to 15 rows
 '''
-# LOE: For copy/paste purposes
-#   data[''] = data[''].replace('NA', '-1')
-#   See: https://stackoverflow.com/questions/38117016/update-pandas-dataframe-with-str-replace-vs-replace
-# Alternative method for replacing
-#   data.fillna(0, inplace=True)
+data = data.replace('NA',0)
+#data = data.fillna(0)
+data = data.dropna()
 '''
 
-# Fill NA with 0 for now 
-# Not sure if this actually works; it didn't seem to work until I changed the code to
-# use the copied csv instead. The main problem AFAIK is the rows of empty commas at the end
-data['sex (1=MtF; 2 =FtM)'] = data['sex (1=MtF; 2 =FtM)'].replace('NA', '0')
-data['initial_sex_orientation (1= androphilic; 2 =gynephilic; 3 = bisexual, 4 = analloerotic)'] = data['initial_sex_orientation (1= androphilic; 2 =gynephilic; 3 = bisexual, 4 = analloerotic)'].replace('NA', '0')
-data['hormontherapy (1 =yes; 2 =no)'] = data['hormontherapy (1 =yes; 2 =no)'].replace('NA', '0')
-data['sex reassignement surgery (1= yes; 2 = no)'] = data['sex reassignement surgery (1= yes; 2 = no)'].replace('NA', '0')
-
+# Post-cleaning message
 print("* NA values have been successfully handled")
 
-# refine variables after handling NA 
-# LOE: not sure what you mean here?
-
-# perform regression 
+# Perform regression 
 regr = linear_model.LinearRegression()
 regr.fit(X,y)
 
@@ -97,3 +101,83 @@ regr.fit(X,y)
 # Result should be 1 (yes) or 2 (no) or in that range
 predictIfChange = regr.predict([[2,3,1,1]]) # Arbitrary for now
 print(predictIfChange)
+
+# SAM: Correlation calculations and visualizations 
+# Selecting columns to be used for correlations matrix 
+columnsOfInterest = [
+    'sex (1=MtF; 2 =FtM)',
+    'initial_sex_orientation (1= androphilic; 2 =gynephilic; 3 = bisexual, 4 = analloerotic)',
+    'hormontherapy (1 =yes; 2 =no)',
+    'sex reassignement surgery (1= yes; 2 = no)',
+    'changesexorient (there has been a change in self-reported sexual orientation: 1= yes; 2 = no)'
+]
+
+# Utilizing corr() function from pandas library to calculate correlations 
+correlationsData = data[columnsOfInterest]
+correlationMatrix = correlationsData.corr()
+print(correlationMatrix)
+styledCorrMatrix = correlationMatrix.style.background_gradient(cmap='coolwarm') # Produces a matrix of correlations with color-coding to appear like a heatmap
+
+# Export dataframe as an HTML file so it can be viewed in web browser
+htmlFilePath = "/workspaces/CS3080FinalProject/correlationMatrix.html"
+with open(htmlFilePath, "w") as f:
+    f.write(styledCorrMatrix.to_html())
+
+# CORRELATION RESULTS AND INTERPRETATIONS
+# changesexorient and sex: 	                        0.158631
+# changesexorient and initial_sex_orientation:      0.123523
+# changesexorient and hormontherapy:                0.074691
+# cchangesexorient and sex_reassignment_surgery:    0.155776
+
+# According to the results of the correlation calculations, there appears to be very weak correlations 
+# between change in self-reported sexual orientation and initial sex, intial sex orientation, whether the 
+# participant is taking hormone therapy, and whether the participant has undergone gender-affirming surgery. 
+
+# ^^ These correlations are consistent with the conclusions of the study "Transgender Transitioning and Change 
+# of Self-Reported Sexual Orientation", which states, "...self-reported change in sexual orientation is a common 
+# phenomenon in transsexual persons. Transition was not directly involved in this change, since a significant 
+# number of participants reported a change in sexual orientation prior to first psychological counseling and 
+# prior to initiation of cross-sex hormone treatment."
+
+# SAM: Z-test (compare change in self-reported sexual orientation between FTM and MTF populations) 
+# Reference for the proportions_ztest function --> https://www.statsmodels.org/stable/generated/statsmodels.stats.proportion.proportions_ztest.html
+# Proportion: percentage of the population group that reports a change in self-reported sexual orientation
+
+# I will be removing NA values (currently 0) here since they are not applicable, and altering the values so that 0 = no and 1 = yes
+ztestData = data[data['changesexorient (there has been a change in self-reported sexual orientation: 1= yes; 2 = no)'] != 0]
+ztestData['changesexorient (there has been a change in self-reported sexual orientation: 1= yes; 2 = no)'] = ztestData['changesexorient (there has been a change in self-reported sexual orientation: 1= yes; 2 = no)'].replace({2: 0})
+
+# Filter data to create variables for FTM and MTF populations 
+MTFData = ztestData[ztestData['sex (1=MtF; 2 =FtM)'] == 1]['changesexorient (there has been a change in self-reported sexual orientation: 1= yes; 2 = no)']
+FTMData = ztestData[ztestData['sex (1=MtF; 2 =FtM)'] == 2]['changesexorient (there has been a change in self-reported sexual orientation: 1= yes; 2 = no)']
+
+# Calculate proportions for each population group (how many 1 values under changesexorient)
+MTFProp = MTFData.mean()
+FTMProp = FTMData.mean()
+print(MTFProp) # 0.338 - meaning 33.8% of MTF study participants reported a change in sexual orientation
+print(FTMProp) # 0.222 - meaning 22.2% of FTM study participants reported a change in sexual orientation
+
+# ^^ These results are mostly consistent with the conclusions of the study:
+# "About one third of MtF (32.9 %, N  =  23) reported a change in sexual orientation during 
+# their life, in contrast to 22.2 % (N  =  10) in the FtM group (n.s.)."
+
+# Count the number of successes and trials for each group
+count = [sum(MTFData == 1), sum(FTMData == 1)] # Number of successes
+nobs = [len(MTFData), len(FTMData)] # Number of observations 
+
+# Perform z-test and save results to a variable 
+zTestResults = proportions_ztest(count, nobs)
+print(zTestResults)
+
+# Z-TEST RESULTS
+# Test statistic: 1.33
+# P-value: 0.18
+
+# Null hypothesis: There is no significant difference in the frequency of change in self-reported sexual orientation
+# between FTM and MTF population groups 
+# Alternative hypothesis: There is a significant difference in the frequency of change in self-reported sexual orientation
+# between FTM and MTF population groups 
+
+# ^^Considering the p-value of the z-test, the result is not significant and the null hypothesis is 
+# accepted. Therefore, there is no significant difference in the frequency of change in self-reported 
+# sexual orientation between FTM and MTF population groups.
